63787f52d5ac7f17160ee0cbacefa9be757eb127
eddie-m-m <eddie.m.menefee@gmail.com>
2023-10-04 20:18:02 -0700

    Instantiate Cli struct

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,10 @@ fn main() {
         pattern: String,
         path: std::path::PathBuf,
     }
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("no path given");
+    let args = Cli {
+        pattern: pattern,
+        path: std::path::PathBuf::from(path),
+    }
 }
